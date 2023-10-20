3f9653578d9509d54e0ed662036dfb57570bec2b
kouhei.umeki <kouhei11.u@gmail.com>
2023-10-20 14:28:21 +0000

    ‘midified’

--- team.rb
+++ team.rb
@@ -1,38 +1,38 @@
 class Team
- attr_accessor :name, :win, :lose, :draw
+  attr_accessor :name, :win, :lose, :draw
   
 def initialize(name,win,lose,draw)
- self.name=name
- self.win=win
- self.lose=lose
-self.draw=draw
+  self.name=name
+  self.win=win
+  self.lose=lose
+  self.draw=draw
 end
  
- def calc_win_rate
+def calc_win_rate
   win/(lose.to_f+win.to_f)
- end
+end
  
- def show_team_result
- puts "#{self.name}の2020年の成績は#{self.win}勝#{self.lose}敗#{self.draw}分、勝率は#{calc_win_rate}です。"
- end
+def show_team_result
+  puts "#{self.name}の2020年の成績は#{self.win}勝#{self.lose}敗#{self.draw}分、勝率は#{calc_win_rate}です。"
+end
 
 end
 
-Giants=Team.new('Giants',67,45,8)
-Tigers=Team.new('Tigers',60,53,7)
-Dragons=Team.new('Dragons',60,53,7)
-Baysters=Team.new('Baysters',56,58,6)
-Carp=Team.new('Carp',52,56,12)
-Swallows=Team.new('Swallows',41,69,10)
+giants=Team.new('Giants',67,45,8)
+tigers=Team.new('Tigers',60,53,7)
+dragons=Team.new('Dragons',60,55,5)
+baysters=Team.new('Baysters',56,58,6)
+carp=Team.new('Carp',52,56,12)
+swallows=Team.new('Swallows',41,69,10)
 
-Giants.show_team_result
+giants.show_team_result
 
-Tigers.show_team_result
+tigers.show_team_result
 
-Dragons.show_team_result
+dragons.show_team_result
 
-Baysters.show_team_result
+baysters.show_team_result
 
-Carp.show_team_result
+carp.show_team_result
 
-Swallows.show_team_result
+swallows.show_team_result
